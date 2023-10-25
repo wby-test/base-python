aa5e28b707973be7bcd09b61502c48d2d8fa118a
wangbaoyi1 <wangbaoyi1@jd.com>
2023-10-25 13:08:14 +0800

    基础类型

---
+++ base/types/base.py
@@ -0,0 +1,23 @@
+# #号表示单行注释
+"""
+双重多引号表示多行注释
+
+"""
+
+
+def print_base_types():
+    a = "a"
+    b = 1
+    print("string: ", a, " int", b)
+
+
+print(1 + 1)
+
+
+def print_base_type_with_struct():
+    print("hello world")
+
+
+if __name__ == '__main__':
+    print_base_types()
+    print_base_type_with_struct()
---
+++ base/types/string.py
@@ -0,0 +1,21 @@
+# 关于字符串string的一些解释
+
+# 单双引号表示字符串的区别以及转义符号的使用
+print(r"c:\casdfa")
+
+# 表达式str 和 repr
+print("xxx\n")
+print(str("xxx\n"))
+print(repr("xxx\n"))
+print(r"xxx\n")
+
+
+# 单引号和双引号区别
+print('xxx\n')
+print("xxx\n")
+print("end")
+
+
+if __name__ == '__main__':
+    print("关于字符串的一些注意事项")
+    print("注意编码相关是否了解")
